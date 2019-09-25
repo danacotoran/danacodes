43afcd7a8770cf9cbe507f8e2a241c53d3781cdd
Dana Cotoran <dana.cotoran@gmail.com>
2019-09-25 14:22:31 +0100

    Attempt to fix Automatic prerendering build warning

--- pages/_app.js
+++ pages/_app.js
@@ -40,15 +40,6 @@ const GlobalStyle = createGlobalStyle`
 `
 
 export default class MyApp extends App {
-  static async getInitialProps({ Component, router, ctx }) {
-    let pageProps = {}
-
-    if (Component.getInitialProps) {
-      pageProps = await Component.getInitialProps(ctx)
-    }
-
-    return { pageProps }
-  }
   render () {
     const { Component, pageProps } = this.props
     return (
