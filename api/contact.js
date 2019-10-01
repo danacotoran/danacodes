2fb2a99628ce52d7099566e90ec395598446fc03
Dana Cotoran <dana.cotoran@gmail.com>
2019-10-01 17:50:29 +0100

    Uninstall express, add node js serverless function to handle contact form submissions

---
+++ api/contact.js
@@ -0,0 +1,22 @@
+
+const mailer = require('../mailer')
+
+module.exports = (req, res) => {
+
+  const { email = '', name = '', message = '' } = req.body
+  if (email.length && name.length && message.length) {
+
+    mailer({ email, name, text: message }).then(() => {
+      res.send('success')
+    }).catch((error) => {
+      res.status(400)
+      console.log('failed', error)
+      res.send('error')
+    })
+  }
+  else {
+    res.status(400)
+      .send('Please ensure all the fields are filled in')
+  }
+
+}
