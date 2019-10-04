d4a8c6dc85d2ee00b81d1e443e25ff3c78023f17
Dana Cotoran <dana.cotoran@gmail.com>
2019-10-04 14:50:58 +0100

    Resolve conflict

--- pages/_document.js
+++ pages/_document.js
@@ -25,7 +25,22 @@ export default class MyDocument extends Document {
         <Head>
           {/*  Output the styles in the head  */}
           {this.props.styleTags}
+          <link rel="shortcut icon" href="/favicon.ico">
 
+          <link rel="icon" type="image/png" href="/favicon-16x16.png" sizes="16x16" />
+          <link rel="icon" type="image/png" href="/favicon-32x32.png" sizes="32x32" />
+          <link rel="icon" type="image/png" href="/favicon-48x48.png" sizes="48x48" />
+          <link rel="icon" type="image/png" href="/favicon-64x64.png" sizes="64x64" />
+          <link rel="icon" type="image/png" href="/favicon-96x96.png" sizes="96x96" />
+          <link rel="icon" type="image/png" href="/favicon-128x128.png" sizes="128x128" />
+          <link rel="icon" type="image/png" href="/favicon-196x196.png" sizes="196x196" />
+
+          <link rel="apple-touch-icon" href="touch-icon-iphone.png" />
+          <link rel="apple-touch-icon" sizes="76x76" href="touch-icon-ipad.png" />
+          <link rel="apple-touch-icon" sizes="120x120" href="touch-icon-iphone-retina.png" />
+          <link rel="apple-touch-icon" sizes="152x152" href="touch-icon-ipad-retina.png" />
+          <link rel="apple-touch-icon" sizes="167x167" href="touch-icon-ipad-pro.png" />
+          <link rel="apple-touch-icon" sizes="180x180" href="touch-icon-iphone-6plus.png" />
           <meta name="description" content="Dana is a front end developer based in London, UK" />
         </Head>
         <body>
